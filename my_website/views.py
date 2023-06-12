c8e1b5f68815d6c35208e518d514e326d09223f7
yongyut <yongyuttgsaving@gmail.com>
2023-06-12 22:40:13 +0700

    test2

--- my_website/views.py
+++ my_website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Record
 
 def home(request):
@@ -53,5 +53,39 @@ def customer_record(request, pk):
     else:
           messages.success(request, 'You must be logged in to view that page')
           return redirect('home')
+
+def delete_record(request, pk):  
+     if request.user.is_authenticated:
+         delete_item = Record.objects.get(id=pk)
+         delete_item.delete()
+         messages.success(request, "Record delete successfully")
+         return redirect('home')
+     else:
+         messages.success(request, "You must be log   to do that")
+         return redirect('home')
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+       if request.method == "POST":
+           if form.is_valid():
+               form.save()
+               messages.success(request, "Record Add ......")
+               return redirect('home')
+       return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in to add record")
+        return redirect('home')
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "You must be logged in to update record")
+        return redirect('home')
 
 # Create your views here.
